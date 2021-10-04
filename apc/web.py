cc6a68a77c2a2ed72bc226b374bba68a24ae41b2
Stefan Kondinski <kondinskis@gmail.com>
2021-10-04 18:31:55 +0200

    adds hourly, daily, monthly aggregations

--- apc/web.py
+++ apc/web.py
@@ -1,7 +1,15 @@
 from flask import Flask, jsonify, render_template, g, request
 
+from sqlalchemy import func, text
+
 from .db import Session, APCReading
 from .config import Config
+
+freq_opts = {
+    "hourly": "%Y-%m-%d %H:00:00.000000",
+    "daily": "%Y-%m-%d 00:00:00.000000",
+    "monthly": "%Y-%m-01 00:00:00.000000",
+}
 
 
 def create_app(config=Config):
@@ -20,11 +28,18 @@ def create_app(config=Config):
 
     @app.get("/")
     def index():
-        readings = g.session.query(APCReading).all()
+        freq = request.args.get("freq", "hourly")
+
+        statement = text(
+            "SELECT id, STRFTIME('{0}', date) as date, AVG(load / no_logs) as load FROM apc_reading GROUP BY STRFTIME('{0}', date)".format(
+                freq_opts.get(freq) or freq_opts["hourly"]
+            )
+        )
+        readings = g.session.query(APCReading).from_statement(statement).all()
         readings_combined = "Date,Load\\n"
         for reading in readings:
             readings_combined = readings_combined + "{},{}\\n".format(
-                reading.date, round(reading.load / reading.no_logs)
+                reading.date, round(reading.load),
             )
         return render_template("index.html", readings=readings_combined)
 
